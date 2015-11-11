9f1708f476bfcd9319f66610b4da7549841cce60
syed <syed.atique@gmail.com>
2015-11-11 23:25:35 +0000

    final commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,24 +9,4 @@ class Ability
       can :read, :all
     end
   end
-
-  # def initialize(user)
-  #   # user ||= User.new
-  #   unless user
-  #     can :read, :all
-  #   else
-  #     if user.role? :admin
-  #        can :manage, :all
-  #      else
-  #        can :read, :all
-  #        can :update, User do |u|
-  #           u.id == user.id
-  #           end
-  #        can :create, Viewing do |v|
-  #           v.user_id == user.id
-  #           end
-  #         can :new, Viewing
-  #      end
-  #     end
-  #   end
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,10 +2,7 @@ class Gig < ActiveRecord::Base
   belongs_to  :genre
   has_many :schedules 
   has_many :venues, through: :schedules
-  has_many :viewings
-  # has_many :users, through: :viewings
 
   mount_uploader :gig_image, GigImageUploader
   
-
 end
